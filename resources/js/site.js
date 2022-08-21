7022dca47717cca5f209ddbd3700bf70838b9ad2
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2022-08-21 22:10:04 +0300

    add header-scroll-sticky

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,3 +1,17 @@
+//header-mobile-icon
 $('#icon-menu').click(function(){
   $('#mobile-nav').toggleClass('hidden block');
+});
+
+//header-scroll-sticky
+$(window).scroll(function(){
+  if ($(window).scrollTop() > 70) {
+    $("#header").addClass("header-sticky");
+    $("#header-nav").removeClass("text-white");
+    $("#site-title").removeClass("md:text-white");
+  }else {
+    $("#header").removeClass("header-sticky");
+    $("#header-nav").addClass("text-white");
+    $("#site-title").addClass("md:text-white");
+  }
 })
